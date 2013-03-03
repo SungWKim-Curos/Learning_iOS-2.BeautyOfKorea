c1a4502a0d7a8f56b593b8de5d5cef78ecb7acfd
sungwkim <sungwkim@curos.co.kr>
2013-03-03 17:48:24 +0900

    Obj-C class MasterBkgrndCtlr 추가

---
+++ BeautyOfKorea/MasterBkgrndCtlr.h
@@ -0,0 +1,13 @@
+//
+//  MasterBkgrndCtlr.h
+//  BeautyOfKorea
+//
+//  Created by curos on 3/3/13.
+//  Copyright (c) 2013 curos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterBkgrndCtlr : UIViewController
+
+@end
