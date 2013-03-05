26d53508c8b22e8b14bd03a1ab7e886052c24007
sungwkim <sungwkim@curos.co.kr>
2013-03-05 21:02:25 +0900

    지도보기 화면에서 뒤로가기 구현

--- BeautyOfKorea/MapCtlr.m
+++ BeautyOfKorea/MapCtlr.m
@@ -35,4 +35,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+-(IBAction) backDidTouch:(UIButton*)a_oSenderBtn
+{
+    [ self.navigationController popViewControllerAnimated:TRUE ] ;
+}
+
+
 @end
